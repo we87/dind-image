52d5001c8bc22bdda3ecba80b740da6a3fbfb4a6
ZhaoBin <antmanler@gmail.com>
2015-12-18 16:14:30 +0800

    Add ss-redir to provision a f**kgfw network

--- run.sh
+++ run.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
-if [[ -z "${USER}" ]] || [[ -z "${SERVER}" ]] || [[ -z "${PASSWORD}" ]]; then
-	echo 'Disable openconnect service'
-	rm -rf /etc/services.d/occlient
-fi
-
 exec /init "$@"
